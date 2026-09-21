7a4d838bbbe0fa639aa1bf47edb9fbd95bbc1b3b
Naumenko Semen <semenkan2009@gmail.com>
2026-09-21 19:51:38 +0300

    Lab01:first program with Environment

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+Console.WriteLine($"Пользователь: {Environment.UserName}");
+Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
